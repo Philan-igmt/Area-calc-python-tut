c8bcc024e6f9b5f7b1adaa4ed75685504e46219e
Keanan083 <philanisithembiso@gmail.com>
2020-03-09 12:02:21 +0200

    this is the second tut 09.03.2020

---
+++ secondTUT.py
@@ -0,0 +1,13 @@
+import math
+# the values of a,b,c,d
+a=4
+b=8
+c=4
+d=10
+# putting the square root
+p=math.sqrt(25)
+# x=math.sqrt(a-3)/((b**2)+(c**2)+(d**2))
+y=(b**2)+(c**2)+(d**2)
+x=math.sqrt(a-3)
+answer=x/y
+print(answer)
